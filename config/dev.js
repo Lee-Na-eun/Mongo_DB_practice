7da8f89609627051446e00a335483830da609e05
Lee Naeun <nanni4287@gmail.com>
2021-07-04 23:28:24 +0900

    second commit

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Naeun:peVSS4c@hnwJ86x@youtube-clone.gtu2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 const mongoose = require("mongoose");
 
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// application/json
+app.use(express.json());
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
 mongoose
   .connect(
+    config.mongoURI,
     "mongodb+srv://Naeun:peVSS4c@hnwJ86x@youtube-clone.gtu2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
@@ -21,6 +36,18 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
